b9321e5f3cb499e82d8b53b097ed5e6277147677
Joshua Eilers <eilers.joshua@gmail.com>
2014-04-27 16:05:17 -0700

    Removed sh files

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-i686-elf-as boot.s -o boot.o
-i686-elf-gcc -c main.c -o main.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o main.o -lgcc
--- image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p isodir
-mkdir -p isodir/boot
-cp myos.bin isodir/boot/myos.bin
-mkdir -p isodir/boot/grub
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
