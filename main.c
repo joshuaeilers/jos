737c01df3978ad244989a6186be28416f3405139
Joshua Eilers <eilers.joshua@gmail.com>
2014-04-27 22:07:27 -0700

    Working on printf

--- main.c
+++ main.c
@@ -4,7 +4,21 @@
 
 void main() {
 	term_init();
-	puts("Hello world!\n");
-	puts("Goodbye world!\n");
+	printf("Hello world!\n", 0);
+	printf("Goodbye world!\n", 0);
+	printf("printf worked %d !\n", 0);
+	printf("printf worked %d !\n", 3);
+	printf("printf worked %d !\n", 4422);
+	printf("printf worked %d !\n", 9000);
+	printf("printf worked %d !\n", 1337);
+	printf("printf worked %d !\n", 4294967295);
+	printf("printf worked %d !\n", 11);
+	printf("printf worked %x !\n", 0);
+	printf("printf worked %x !\n", 3);
+	printf("printf worked %x !\n", 4422);
+	printf("printf worked %x !\n", 9000);
+	printf("printf worked %x !\n", 1337);
+	printf("printf worked %x !\n", 4294967295);
+	printf("printf worked %x !\n", 17);
 }
 
--- string.c
+++ string.c
@@ -1,12 +1,77 @@
 #include "string.h"
 #include "term.h"
 
+size_t strlen(const char *s) {
+	size_t i = 0;
+
+	while (s[i]) {
+		i++;
+	}
+
+	return i;
+}
+
+size_t itoa(char *buf, uint32_t x) {
+	size_t i = 0;
+	size_t j = 0;
+	char n;
+	char tmp[11]; // 4294967295 (10 digits) is longest uint32_t
+
+	// only handling positive integers for now
+
+	do {
+		n = x % 10;
+		tmp[i++] = n + '0';
+	} while (x /= 10);
+
+	while (i) {
+		buf[j++] = tmp[--i];
+	}
+
+	buf[j] = '\0';
+
+	return j;
+}
+
+size_t itoh(char *buf, uint32_t x) {
+	char hex[16] = "012345789ABCDEF";
+	size_t i = 0;
+	size_t j = 0;
+	size_t n;
+	char tmp[11]; // FFFFFFFF (8 digits) is longest uint32_t
+
+	// only handling positive integers for now
+
+	buf[j++] = '0';
+	buf[j++] = 'x';
+
+	do {
+		n = x % 16;
+		tmp[i++] = hex[n - 1];
+	} while (x /= 16);
+
+	while (i) {
+		buf[j++] = tmp[--i];
+	}
+
+	buf[j] = '\0';
+
+	return j;
+}
+
 void putchar(char c) {
 	if (c == '\n') {
+		/*
+		 * Check if end of screen is reached
+		 */
 		term_row++;
 		term_col = 0;
 		return;
 	}
+
+	/*
+	 * Check if end of screen is reached
+	 */
 
 	term_buf[term_row * VGA_WIDTH + term_col] = term_rgb << 8 | c;
 
@@ -28,12 +93,23 @@ void puts(const char *s) {
 	}
 }
 
-size_t strlen(const char *s) {
-	size_t i = 0;
+void printf(const char *s, uint32_t x) {
+	size_t i;
+	size_t len = strlen(s);
+	char buf[16];
 
-	while (s[i]) {
-		i++;
+	for (i = 0; i < len; i++) {
+		if (s[i] == '%') {
+			++i;
+			if (s[i] == 'd') {
+				itoa(buf, x);
+				puts(buf);
+			} else if (s[i] == 'x') {
+				itoh(buf, x);
+				puts(buf);
+			}
+		} else {
+			putchar(s[i]);
+		}
 	}
-
-	return i;
 }
